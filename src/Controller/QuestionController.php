fc0dfb951ad1582f53bf0bcf81feb4220042d53e
fz <phiane.d@gmail.com>
2024-01-17 15:21:20 +0100

    make User, entity User

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,25 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\Question;
+use App\Form\CommentType;
 use App\Form\QuestionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/question', name: 'question_')]
 class QuestionController extends AbstractController
 {
-    #[Route('/ask', name: 'form')]
-    public function index(Request $request): Response
+    #[Route('/question/ask', name: 'question_form')]
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
 
-        $formQuestion = $this->createForm(QuestionType::class);
+        $question = new Question();
+        $formQuestion = $this->createForm(QuestionType::class, $question);
 
         $formQuestion->handleRequest($request);
 
         if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
-            dd($formQuestion->getData());
+            $question->setNbrOfResponse(0);
+            $question->setRating(0);
+            $question->setCreatedAt(new \DateTimeImmutable());
+            $em->persist($question);
+            $em->flush();
+            $this->addFlash('success', 'Votre question a été ajoutée');
+            return $this->redirectToRoute('home');
         }
 
         return $this->render('question/index.html.twig', [
@@ -28,23 +38,44 @@ class QuestionController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'show')]
-    public function show(Request $request, string $id): Response
+    #[Route('/question/{id}', name: 'question_show')]
+    public function show(Request $request, Question $question, EntityManagerInterface $em): Response
     {
-
-        $question =   [
-            'title' => 'Je suis une super question',
-            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, adipisci. Libero aperiam dolores excepturi, quidem maxime accusantium inventore. Illum, odio dolores! Ullam omnis veritatis laborum, animi inventore nostrum optio voluptates.',
-            'rating' => 20,
-            'author' => [
-                'name' => 'Jean Dupont',
-                'avatar' => 'https://randomuser.me/api/portraits/men/52.jpg'
-            ],
-            'nbrOfResponse' => 15
-        ];
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentType::class, $comment);
+        $commentForm->handleRequest($request);
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            $comment->setCreatedAt(new \DateTimeImmutable());
+            $comment->setRating(0);
+            $comment->setQuestion($question);
+            $question->setNbrOfResponse($question->getNbrOfResponse() + 1);
+            $em->persist($comment);
+            $em->flush();
+            $this->addFlash('success', 'Votre réponse a bien été ajoutée');
+            return $this->redirect($request->getUri());
+        }
 
         return $this->render('question/show.html.twig', [
             'question' => $question,
+            'form' => $commentForm->createView()
         ]);
+    }
+
+    #[Route('/question/rating/{id}/{score}', name: 'question_rating')]
+    public function ratingQuestion(Request $request, Question $question, int $score, EntityManagerInterface $em)
+    {
+        $question->setRating($question->getRating() + $score);
+        $em->flush();
+        $referer = $request->server->get('HTTP_REFERER');
+        return $referer ? $this->redirect($referer) : $this->redirectToRoute('home');
+    }
+
+    #[Route('/comment/rating/{id}/{score}', name: 'comment_rating')]
+    public function ratingComment(Request $request, Comment $comment, int $score, EntityManagerInterface $em)
+    {
+        $comment->setRating($comment->getRating() + $score);
+        $em->flush();
+        $referer = $request->server->get('HTTP_REFERER');
+        return $referer ? $this->redirect($referer) : $this->redirectToRoute('home');
     }
 }
